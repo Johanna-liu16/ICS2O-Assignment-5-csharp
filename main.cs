6c95de7bf786f7e5bfbe60f1752dd57e23d9e9c1
Johanna Liu <99354992+Johanna-liu16@users.noreply.github.com>
2022-06-19 00:59:56 +0000

    whitespacing

--- main.cs
+++ main.cs
@@ -22,27 +22,26 @@ class Program
         Console.WriteLine("");
         Console.Write("Enter a number: ");
         userInput = Convert.ToInt32(Console.ReadLine());
-
-      // process
-      while (counter < userInput)
-      {
-        if (counter % 2 == 0)
+        // process
+        while (counter < userInput)
         {
-          denominator = counter * 2 + 1;
-          answer += num / denominator;
-        }
-        else
-        {
-          denominator = counter * 2 + 1;
-          answer += -(num / denominator);
-        }
+            if (counter % 2 == 0)
+            {
+             denominator = counter * 2 + 1;
+               answer += num / denominator;
+            }
+            else
+            {
+             denominator = counter * 2 + 1;
+               answer += -(num / denominator);
+            }
 
-        counter++;
-      }
-      // output
-      Console.WriteLine("");
-      Console.WriteLine("The value of π is " + answer);
+            counter++;
+        }
+        // output
+        Console.WriteLine("");
+        Console.WriteLine("The value of π is " + answer);
 
-      Console.WriteLine("\nDone.");
+        Console.WriteLine("\nDone.");
     }
 }
